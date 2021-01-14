ffdd17e4025389ed0648e15dbed5e2a175cceb05
AloneH <admin@oibit.cn>
2021-01-14 17:18:44 +0800

    feat: add decorators for controller

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['@babel/preset-env'],
-  plugins: ['@babel/plugin-transform-runtime']
+  plugins: ['@babel/plugin-transform-runtime', ['@babel/plugin-proposal-decorators', { legacy: true }], '@babel/plugin-proposal-class-properties']
 }
