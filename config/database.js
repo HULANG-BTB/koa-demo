5b8a12c868ddf5fe84d22bfef878415c852b56ed
AloneH <admin@oibit.cn>
2021-01-18 16:19:19 +0800

    feat: config for database

---
+++ config/database.js
@@ -0,0 +1,21 @@
+export const database = {
+  host: 'localhost',
+  port: '3306',
+  username: 'root',
+  password: 'root',
+  database: 'koa-demo',
+  dialect: 'mysql',
+  timezone: '+08:00',
+  pool: {
+    max: 10
+  },
+  additional: {
+    timestamps: true, // 自动时间戳
+    createdAt: 'create_time', // 指定创建事件字段
+    updatedAt: 'update_time', // 指定更新事件字段
+    version: 'version', // 乐观锁定机制
+    deletedAt: 'delete_time' // 逻辑删除（软删除）
+  }
+}
+
+export default database
